fb2c47c07552e924e55f9a54d73a0d7899bb08d6
adria <littleivanitsky@gmail.com>
2022-03-28 15:54:16 -0700

    exercise 8 and 9 tests passing

--- exercise-7/exercise-7.sql
+++ exercise-7/exercise-7.sql
@@ -1,2 +1,16 @@
 -- customer ranking by total film time watched
 -- similar to exercise 6
+
+SELECT
+  customer.email,
+  RANK() OVER (ORDER BY SUM(film.length) DESC)
+FROM
+  rental
+INNER JOIN inventory
+ON inventory.inventory_id = rental.inventory_id
+LEFT JOIN film
+ON film.film_id = inventory.film_id
+INNER JOIN customer
+ON customer.customer_id = rental.customer_id
+GROUP BY customer.email
+ORDER BY rank ASC;
